a5ab6765a1e33b24c4c7d23245fd456569c31d5d
michael <mvasilyev04@gmail.com>
2025-05-03 03:38:33 +0300

    remove screen initialization from main func to class Window

--- src/main/java/dev/mishgun/minesweeper/MineSweeper.java
+++ src/main/java/dev/mishgun/minesweeper/MineSweeper.java
@@ -1,13 +1,9 @@
 package dev.mishgun.minesweeper;
 
-import javax.swing.*;
-
 public class MineSweeper 
 {
     public static void main( String[] args )
     {
-        JFrame frame = new JFrame("MineSweeper");
-        frame.setSize(720,480);
-        frame.setVisible(true);
+        new Window();
     }
 }
