89eb084814134841f0933ed3d84e88abaa2c110a
michael <mvasilyev04@gmail.com>
2025-05-03 03:39:19 +0300

    created class Window, initialization now in constructor

---
+++ src/main/java/dev/mishgun/minesweeper/Window.java
@@ -0,0 +1,15 @@
+package dev.mishgun.minesweeper;
+
+import javax.swing.JFrame;
+
+public class Window {
+    public Window() {
+        JFrame frame = new JFrame("MineSweeper");
+        frame.setSize(1280,720);
+        frame.setVisible(true);
+    }
+
+    public void drawArea(int rows, int cols) {
+        
+    }
+}
