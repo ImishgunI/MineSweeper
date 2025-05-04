fba99529bb5e59bfe90c1b3f7eaf3525ff9f3019
michael <mvasilyev04@gmail.com>
2025-05-05 00:22:05 +0300

    added func chooseAnImage to take image by size

--- src/main/java/dev/mishgun/minesweeper/Application.java
+++ src/main/java/dev/mishgun/minesweeper/Application.java
@@ -14,8 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Application extends JFrame {
-    private final int gapBetweenButton = 8;
-    private final int buttonSize = 30;
+    private final int gapBetweenButton = 2;
     private JPanel panel;
     private URL imageURL;
 
@@ -28,18 +27,20 @@ public class Application extends JFrame {
     }
 
     public void drawArea(int rows, int cols) {
+        final int buttonSize = setButtonSize(rows, cols);
         JPanel gridPanel = new JPanel(new GridLayout(rows, cols, gapBetweenButton, gapBetweenButton));
         gridPanel.setPreferredSize(new Dimension(
             cols * (buttonSize + gapBetweenButton),
             rows * (buttonSize + gapBetweenButton)
         ));
         gridPanel.revalidate();
-        imageURL = getClass().getClassLoader().getResource("images/unopened_square.svg.png");
+        String size = chooseAnImage(buttonSize);
+        imageURL = getClass().getClassLoader().getResource("images/unopened_square" + size);
         for(int i = 0; i < rows * cols; i++) {
             JButton button = new JButton();
             button.setPreferredSize(new Dimension(buttonSize, buttonSize));
             button.setIcon(new ImageIcon(imageURL));
-            addActionForButton(button);
+            addActionForButton(button, buttonSize);
             gridPanel.add(button);
         }
         panel.add(gridPanel);
@@ -47,19 +48,47 @@ public class Application extends JFrame {
         setVisible(true);
     }
 
-    private void addActionForButton(JButton b) {
+    private void addActionForButton(JButton b, final int buttonSize) {
+        String size = chooseAnImage(buttonSize);
         b.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if(SwingUtilities.isLeftMouseButton(e)) {
-                    imageURL = getClass().getClassLoader().getResource("images/empty_fill.svg.png");
+                    imageURL = getClass().getClassLoader().getResource("images/empty_fill" + size);
                     b.setIcon(new ImageIcon(imageURL));
                 } else if(SwingUtilities.isRightMouseButton(e)){
-                    imageURL = getClass().getClassLoader().getResource("images/Minesweeper_flag.svg.png");
+                    imageURL = getClass().getClassLoader().getResource("images/Minesweeper_flag" + size);
                     b.setIcon(new ImageIcon(imageURL));
                 }
             }
         });
     }
 
+    private final int setButtonSize(int rows, int cols) {
+        int buttonSize = 0;
+        switch (rows + cols) {
+            case 16:
+                buttonSize = 70;
+                break;
+            case 32:
+                buttonSize = 50;
+                break;
+            case 46:
+                buttonSize = 30;
+                break;
+            default:
+                System.err.println("This size aren't supporting");
+                break;
+        }
+        return buttonSize;
+    }
+
+    private String chooseAnImage(final int buttonSize) {
+        String size_30 = "_30.svg.png";
+        String size_50 = "_50.svg.png";
+        String size_70 = "_70.svg.png";
+        return (buttonSize == 30) ? size_30 : (buttonSize == 50) ? size_50 : (buttonSize == 70) ? size_70 : size_50;
+    }
 }
+
+// TODO: Необходимо сделать функцию которая будет заполнять поле всеми иконками 
