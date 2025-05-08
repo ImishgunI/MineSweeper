6f29e3481776a92ce1330233e22d89657346954e
michael <mvasilyev04@gmail.com>
2025-05-09 02:08:15 +0300

    did a lot of things, added mines in grid and also worked with buttons action

--- src/main/java/dev/mishgun/minesweeper/Application.java
+++ src/main/java/dev/mishgun/minesweeper/Application.java
@@ -6,7 +6,9 @@ import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,6 +19,7 @@ public class Application extends JFrame {
     private final int gapBetweenButton = 2;
     private JPanel panel;
     private URL imageURL;
+    private ArrayList<JButton> buttons = new ArrayList<JButton>();
 
     public Application() {
         setTitle("MineSweeper");
@@ -40,9 +43,12 @@ public class Application extends JFrame {
             JButton button = new JButton();
             button.setPreferredSize(new Dimension(buttonSize, buttonSize));
             button.setIcon(new ImageIcon(imageURL));
-            addActionForButton(button, buttonSize);
-            gridPanel.add(button);
+            button.putClientProperty("unopened", true);
+            buttons.add(button);
         }
+        ArrayList <JButton> shuffled = addBombAtButton(buttons, rows, cols, buttonSize);
+        addButtonsAtPanel(shuffled, gridPanel);
+        setActionOnButton(shuffled, buttonSize);
         panel.add(gridPanel);
         pack();
         setVisible(true);
@@ -53,18 +59,37 @@ public class Application extends JFrame {
         b.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(SwingUtilities.isLeftMouseButton(e)) {
-                    imageURL = getClass().getClassLoader().getResource("images/empty_fill" + size);
-                    b.setIcon(new ImageIcon(imageURL));
-                } else if(SwingUtilities.isRightMouseButton(e)){
-                    imageURL = getClass().getClassLoader().getResource("images/Minesweeper_flag" + size);
-                    b.setIcon(new ImageIcon(imageURL));
+                if(SwingUtilities.isLeftMouseButton(e) && b.getClientProperty("flag") == null) {
+                    if(b.getClientProperty("mine") != null) {
+                        imageURL = getClass().getClassLoader().getResource("images/fail" + size);
+                        b.setIcon(new ImageIcon(imageURL));
+                        // try {
+                        //     Thread.sleep();
+                        // } catch(Exception ex) {
+                        //     System.err.println(ex.getMessage());
+                        // }
+                    } else {
+                        imageURL = getClass().getClassLoader().getResource("images/empty_fill" + size);
+                        b.setIcon(new ImageIcon(imageURL));
+                        b.putClientProperty("empty_fill", true);
+                    }
+                } else if(SwingUtilities.isRightMouseButton(e)) {
+                    if(b.getClientProperty("empty_fill") == null || (b.getClientProperty("unopened") != null 
+                        && b.getClientProperty("mine") != null)) {
+                            imageURL = getClass().getClassLoader().getResource("images/Minesweeper_flag" + size);
+                            b.setIcon(new ImageIcon(imageURL));
+                            b.putClientProperty("flag", true);
+                    } else if(b.getClientProperty("flag") != null && (b.getClientProperty("empty_fill") == null)){
+                        imageURL = getClass().getClassLoader().getResource("images/unopened_square" + size);
+                        b.setIcon(new ImageIcon(imageURL));
+                        b.putClientProperty("unopened", true);
+                    }
                 }
             }
         });
     }
 
-    private final int setButtonSize(int rows, int cols) {
+    private int setButtonSize(int rows, int cols) {
         int buttonSize = 0;
         switch (rows + cols) {
             case 16:
@@ -89,6 +114,39 @@ public class Application extends JFrame {
         String size_70 = "_70.svg.png";
         return (buttonSize == 30) ? size_30 : (buttonSize == 50) ? size_50 : (buttonSize == 70) ? size_70 : size_50;
     }
-}
 
-// TODO: Необходимо сделать функцию которая будет заполнять поле всеми иконками 
+    private ArrayList<JButton> addBombAtButton(ArrayList<JButton> list, int rows, int cols, final int buttonSize) throws IndexOutOfBoundsException {
+        String size = chooseAnImage(buttonSize);
+        float difficult = (float) (0.156 + (0.206 - 0.156) * ((rows * cols - 64) / (480.0 - 64)));
+        ArrayList<JButton> shuffled = new ArrayList<JButton>(Arrays.asList(new JButton[rows * cols]));
+        fillArray(list, shuffled);
+        Random r = new Random();
+        byte amount = (byte)(Math.round(rows * cols * difficult));
+        imageURL = getClass().getClassLoader().getResource("images/unopened_square" + size);
+        for (int i = 0; i < amount + 1; i++) {
+            JButton b = new JButton(new ImageIcon(imageURL));
+            b.putClientProperty("mine", true);
+            int value = r.nextInt(list.size() - 1);
+            shuffled.set(value, b);
+        }
+        return shuffled;
+    }
+
+    private void fillArray(ArrayList<JButton> list, ArrayList<JButton> shuffled) {
+        for (int i = 0; i < list.size(); i++) {
+            shuffled.set(i, list.get(i));
+        }
+    }
+
+    private void addButtonsAtPanel(ArrayList<JButton> buttons, JPanel gridPanel) {
+        for(JButton b : buttons) {
+            gridPanel.add(b);
+        }
+    }
+
+    private void setActionOnButton(ArrayList <JButton> shuffled, final int buttonSize) {
+        for(int i = 0; i < shuffled.size(); i++) {
+            addActionForButton(shuffled.get(i), buttonSize);
+        }
+    }
+}
