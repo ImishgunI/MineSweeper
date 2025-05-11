0b9c067d6a40e27cd2f9aaa741397f255d8052a9
michael <mvasilyev04@gmail.com>
2025-05-12 02:47:56 +0300

    added numbers on grid

--- src/main/java/dev/mishgun/minesweeper/Application.java
+++ src/main/java/dev/mishgun/minesweeper/Application.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,6 +25,7 @@ public class Application extends JFrame {
     private URL imageURL;
     private ArrayList<JButton> buttons = new ArrayList<JButton>();
     private HashSet<Integer> hs = new HashSet<>();
+    private Queue<JButton> zerosButtons = new LinkedList<>();
 
     public Application() {
         setTitle("MineSweeper");
@@ -51,6 +54,7 @@ public class Application extends JFrame {
         }
         ArrayList <JButton> shuffled = addBombAtButton(buttons, rows, cols);
         addButtonsAtPanel(shuffled, gridPanel);
+        addNumbersAtGrid(shuffled, rows, cols);
         setActionOnButton(shuffled, buttonSize);
         panel.add(gridPanel);
         pack();
@@ -72,6 +76,12 @@ public class Application extends JFrame {
                         //     System.err.println(ex.getMessage());
                         // }
                         b.putClientProperty("mine_failed", true);
+                        b.putClientProperty("unopened", null);
+                    } else if(b.getClientProperty("number") != null) {
+                        Integer count = (Integer) b.getClientProperty("number");
+                        imageURL = getImageNumber(count, buttonSize);
+                        b.setIcon(new ImageIcon(imageURL));
+                        b.putClientProperty("empty_fill", null);
                         b.putClientProperty("unopened", null);
                     } else {
                         imageURL = getClass().getClassLoader().getResource("images/empty_fill" + size);
@@ -170,5 +180,75 @@ public class Application extends JFrame {
         for(int i = 0; i < shuffled.size(); i++) {
             addActionForButton(shuffled.get(i), buttonSize);
         }
+    }
+
+    private void addNumbersAtGrid(ArrayList<JButton> list, int rows, int cols) {
+        JButton b = null;
+        for(int i = 0; i < list.size() - 1; i++) {
+            int row = i / cols;
+            int col = i % cols;
+            b = list.get(i);
+            if(b.getClientProperty("mine") == null) {
+                int count = calculateNumberForButton(row, col, rows, cols, list);
+                if(count != 0) {
+                    b.putClientProperty("number", count);
+                }
+            }
+        }
+    }
+
+    private int calculateNumberForButton(int row, int col, int rows, int cols, ArrayList<JButton> list) {
+        JButton b = null;
+        int count = 0;
+        for(int dr = -1; dr <= 1; dr++) {
+            for(int dc = -1; dc <= 1; dc++) {
+                if(dr == 0 && dc == 0) continue;
+                int nr = row + dr;
+                int nc = col + dc;
+
+                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                    int index = nr * cols + nc;
+                    b = list.get(index);
+                    if(b.getClientProperty("mine") != null) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private URL getImageNumber(int count, int size) {
+        String len = chooseAnImage(size);
+        switch (count) {
+            case 1:
+                imageURL = getClass().getClassLoader().getResource("images/number_1" + len);
+                break;
+            case 2:
+                imageURL = getClass().getClassLoader().getResource("images/number_2" + len);
+                break;
+            case 3:
+                imageURL = getClass().getClassLoader().getResource("images/number_3" + len);
+                break;
+            case 4:
+                imageURL = getClass().getClassLoader().getResource("images/number_4" + len);
+                break;
+            case 5:
+                imageURL = getClass().getClassLoader().getResource("images/number_5" + len);
+                break;
+            case 6:
+                imageURL = getClass().getClassLoader().getResource("images/number_6" + len);
+                break;
+            case 7:
+                imageURL = getClass().getClassLoader().getResource("images/number_7" + len);
+                break;
+            case 8:
+                imageURL = getClass().getClassLoader().getResource("images/number_8" + len);
+                break;
+            default:
+                System.err.println("Incorrect count: " + count);
+                break;
+        }
+        return imageURL;
     }
 }
